9307e1d41c83ac09fdb772c12bd95e1407860644
libra guest <abrady0@yahoo.com>
2012-08-29 02:31:21 -0400

    jade example

--- server/server.js
+++ server/server.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
+var jade = require('jade');
+
+// Compile a function
+var options = {
+//    self Use a self namespace to hold the locals (false by default)
+//locals Local variable object
+//    filename : "server/server.js",
+    debug : true, //  Outputs tokens and function body generated
+// compiler Compiler to replace jade's default
+    compileDebug : process.env.debug, // When false no debug instrumentation is compiled
+    pretty : true // Add pretty-indentation whitespace to output (false by default)
+};
 
 app.get('/', function(req, res){
-  // res.send('Hello World');
+    var locals = { name : 'aaron'};
+    // recompiling for iteration reasons, should be outside
+    var jade_fn = jade.compile(
+        'p Hello #{name}',
+        options);
+  
+    var response = jade_fn(locals);
+
+    res.send(response);
 });
 
 
